1c04e14c18ddc593c84ca587fcaef54735fdd0fc
Kirby Robinson <kirbyrobins@gmail.com>
2025-07-11 15:08:57 -0400

    banner app store link

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -10,7 +10,11 @@ const Banner = () => (
       title="Rediscover your best memories in beautifully sorted folders."
       subtitle="Visit the App Store."
       button={
-        <Link href="/">
+        <Link
+          href="https://apps.apple.com/app/id6747405112"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Button>Get Started</Button>
         </Link>
       }
